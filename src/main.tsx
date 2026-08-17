4b712703f4d5be1c373770fa33dd323a771a6ff4
engnaderkamel1-sudo <eng.naderkamel1@gmail.com>
2026-08-17 17:56:44 +0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// مسح أي Service Worker قديم وإلغاء تسجيله لتجنب مشكلة الكاش (الريفريش مرتين)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
