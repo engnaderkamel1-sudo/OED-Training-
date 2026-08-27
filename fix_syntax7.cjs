345038320b614132f8ac3e296b083e69a536342f
engnaderkamel1-sudo <eng.naderkamel1@gmail.com>
2026-08-27 23:38:47 +0300

    Clean up obsolete file: fix_syntax7.cjs

--- fix_syntax7.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/components/AdminDashboard.tsx', 'utf8');
-
-// A simple way to check tag balance:
-let divOpenCount = (content.match(/<div(\s|>)/g) || []).length;
-let divCloseCount = (content.match(/<\/div>/g) || []).length;
-console.log(`Open div: ${divOpenCount}, Close div: ${divCloseCount}`);
