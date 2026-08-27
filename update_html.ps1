1336f0ff74b30beb6881b23ad2d338dafc65e77c
engnaderkamel1-sudo <eng.naderkamel1@gmail.com>
2026-08-27 23:39:41 +0300

    Clean up obsolete file: update_html.ps1

--- update_html.ps1
+++
@@ -1,6 +0,0 @@
-$content = Get-Content -Path "C:\Users\nader.reda\Downloads\oed-training-management-system\index.html" -Raw
-$pattern = '(?s)</head>'
-$replace = '  <script src="https://unpkg.com/html5-qrcode"></script>
-  </head>'
-$content = $content -replace $pattern, $replace
-$content | Set-Content -Path "C:\Users\nader.reda\Downloads\oed-training-management-system\index.html" -Encoding UTF8
